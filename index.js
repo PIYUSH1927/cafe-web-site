787091eded9b54af26e3371c7be21e4925c2f3a3
Piyush <90149415+PIYUSH1927@users.noreply.github.com>
2023-01-26 00:11:10 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-
-// Get the element with the ID "menu"
-var menu = document.getElementById("menu");
-
-// Add a click event listener to the menu
-menu.addEventListener("click", function() {
-  // When the menu is clicked, toggle the class "open"
-  menu.classList.toggle("open");
-});
